b5564451f1ca73ad15599b67984381e29a475ec9
Alex Biehl <alexbiehl@gmail.com>
2015-11-26 10:44:48 +0100

    Insert, query, update

--- src/Bandits/Backend/HRR.hs
+++ src/Bandits/Backend/HRR.hs
@@ -16,9 +16,11 @@ import           Bandits.Experiment.Types
 import           Control.Monad.Free
 import           Data.Convertible
 import           Database.HDBC.Query.TH
+import qualified Database.HDBC.Record.Insert        as Insert
 import           Database.HDBC.Record.Query
 import           Database.HDBC.Record.Statement
 import           Database.HDBC.Record.TH
+import qualified Database.HDBC.Record.Update        as Update
 import           Database.HDBC.SqlValue
 import           Database.HDBC.Types                (IConnection)
 import           Database.Record.Persistable
@@ -37,6 +39,7 @@ instance Convertible Variation SqlValue where
   safeConvert (Variation s) = safeConvert s
 $(derivePersistableInstanceFromValue [t| Variation |])
 deriving instance PersistableWidth Variation
+deriving instance ShowConstantTermsSQL Variation
 
 instance Convertible SqlValue ExperimentId where
   safeConvert s = ExperimentId <$> safeConvert s
@@ -44,6 +47,7 @@ instance Convertible ExperimentId SqlValue where
   safeConvert (ExperimentId s) = safeConvert s
 $(derivePersistableInstanceFromValue [t| ExperimentId |])
 deriving instance PersistableWidth ExperimentId
+deriving instance ShowConstantTermsSQL ExperimentId
 
 instance Convertible SqlValue UserId where
   safeConvert s = UserId <$> safeConvert s
@@ -51,6 +55,7 @@ instance Convertible UserId SqlValue where
   safeConvert (UserId s) = safeConvert s
 $(derivePersistableInstanceFromValue [t| UserId |])
 deriving instance PersistableWidth UserId
+deriving instance ShowConstantTermsSQL UserId
 
 instance Convertible SqlValue Reward where
   safeConvert s = Reward <$> safeConvert s
@@ -58,6 +63,7 @@ instance Convertible Reward SqlValue where
   safeConvert (Reward s) = safeConvert s
 $(derivePersistableInstanceFromValue [t| Reward |])
 deriving instance PersistableWidth Reward
+deriving instance ShowConstantTermsSQL Reward
 
 -- Table definition for relational record.
 $(defineTableDefault
@@ -70,30 +76,12 @@ $(defineTableDefault
   [ ("as_experiment_id", [t| ExperimentId |])
   , ("as_user_id", [t| UserId |])
   , ("as_variation", [t| Variation |])
+  , ("as_reward", [t| Reward |])
   ]
   -- derivings
   [toConName "Eq", toConName "Show", toConName "Generic"]
   -- primary key columns
   [0, 1]
-  -- not null column?
-  Nothing
- )
-
-$(defineTableDefault
-  defaultConfig
-  -- schema name
-  "bandits"
-  -- table name
-  "reward1"
-  -- columns
-  [ ("r_experiment_id", [t| ExperimentId |])
-  , ("r_user_id", [t| UserId |])
-  , ("r_reward", [t| Reward |])
-  ]
-  -- derivings
-  [toConName "Eq", toConName "Show", toConName "Generic"]
-  -- primary key columns
-  [0,1,2]
   -- not null column?
   Nothing
  )
@@ -110,9 +98,11 @@ runExperiment m conn = iterM run m
       a <- queryAssignment eid uid conn
       k a
     run (NewAssignment eid uid k) = do
-      undefined
+      insertAssignment' eid uid undefined conn
+      k undefined
     run (NewReward eid uid rew k) = do
-      undefined
+      updateReward eid uid rew conn
+      k
 
 -- | Queries the database
 queryAssignment :: ExperimentId -> UserId -> RunHRRBackend (Maybe Variation)
@@ -122,5 +112,25 @@ queryAssignment eid uid conn = do
   ma <- fetchUnique' es
   return $ asVariation <$> ma
 
-insertReward :: ExperimentId -> UserId -> Reward -> RunHRRBackend ()
-insertReward eid uid rew = undefined
+updateReward :: ExperimentId -> UserId -> Reward -> RunHRRBackend ()
+updateReward eid uid rew conn = do
+  ps <- Update.prepareUpdate conn upd
+  _  <- Update.runPreparedUpdate ps ()
+  return ()
+  where
+    upd =
+      typedUpdate tableOfAssignment . updateTarget $ \proj -> do
+        asReward' <-# value rew
+        wheres $ proj ! asExperimentId' .=. value eid
+        wheres $ proj ! asUserId' .=. value uid
+        wheres $ proj ! asReward' .=. value (Reward 0.0)
+
+insertAssignment' :: ExperimentId -> UserId -> Variation -> RunHRRBackend ()
+insertAssignment' eid uid var conn = do
+  ps <- Insert.prepareInsert conn insertAssignment
+  _  <- Insert.runPreparedInsert ps Assignment { asExperimentId = eid
+                                               , asUserId = uid
+                                               , asVariation = var
+                                               , asReward = Reward 0.0
+                                               }
+  return ()
