0b5ae795045f0b354047d104e879eabd405e42df
Alex Biehl <abiehl@novomind.com>
2015-12-01 09:57:32 +0100

    Use Sha256 and 12 bytes for experiment id.

--- src/Bandits/Experiment.hs
+++ src/Bandits/Experiment.hs
@@ -5,20 +5,20 @@ module Bandits.Experiment where
 import           Bandits.Experiment.Instructions
 import           Bandits.Experiment.Types
 
-import           Crypto.Hash                     (Digest, SHA1)
+import           Crypto.Hash                     (Digest, SHA256)
 import qualified Crypto.Hash                     as Cryptonite
 import qualified Data.ByteArray                  as ByteArray
 import qualified Data.ByteArray.Encoding         as ByteArray
 import qualified Data.Text.Encoding              as Text
 
 -- | Determistically create an ExperimentId from a Experiment.
---   It hashes the experiment name and takes the first 8 hex encoded
+--   It hashes the experiment name and takes the first 12 hex encoded
 --   bytes.
 mkExperimentId :: Experiment -> ExperimentId
 mkExperimentId Experiment{..} = MkExperimentId (Text.decodeUtf8 hex)
   where
-    hash = Cryptonite.hash (Text.encodeUtf8 expName) :: Digest SHA1
-    hex = ByteArray.take 8 (ByteArray.convertToBase ByteArray.Base16 hash)
+    hash = Cryptonite.hash (Text.encodeUtf8 expName) :: Digest SHA256
+    hex = ByteArray.take 12 (ByteArray.convertToBase ByteArray.Base16 hash)
 
 -- | Assigns a variation to a user belonging to an experiment.
 assign :: RunExperiment m => ExperimentId -> UserId -> m Variation
